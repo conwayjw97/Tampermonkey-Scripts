06def36920be6d4593367f5fc385051c61049e6c
James <39833325+conwayjw97@users.noreply.github.com>
2019-12-31 14:51:16 +0100

    Now runs on each page load

--- YouTube-Customiser.js
+++ YouTube-Customiser.js
@@ -5,17 +5,13 @@
 // @description  Remove the big ad banner from YouTube's homepage
 // @author       James Conway
 // @match        https://www.youtube.com/*
-// @grant        none
+// @grant        GM_log
 // @require      http://code.jquery.com/jquery-latest.js
+// @run-at       document-start
 // ==/UserScript==
 
-(function() {
-    'use strict';
+GM_log("YouTube Customiser is running");
 
-    console.log("YouTube Customiser is running");
-
-    if(window.location.href == "https://www.youtube.com/"){
-        document.getElementById("masthead-ad").remove();
-        window.location.replace("https://www.youtube.com/feed/subscriptions");
-    }
-})();
+if(window.location.href == "https://www.youtube.com/"){
+    window.location.replace("https://www.youtube.com/feed/subscriptions");
+}
