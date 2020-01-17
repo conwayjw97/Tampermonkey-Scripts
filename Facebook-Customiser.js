85a41f437de398df42750533fbed92870e2c5261
James <39833325+conwayjw97@users.noreply.github.com>
2020-01-17 14:02:28 +0000

    Create Facebook-Customiser.js

---
+++ Facebook-Customiser.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         James's Facebook Customiser
+// @namespace    com.conwayjw97.fbcustomiser
+// @version      0.1
+// @author       James Conway
+// @match        https://www.facebook.com/*
+// @grant        GM_log
+// @run-at       document-start
+// ==/UserScript==
+
+GM_log("Facebook Customiser is running");
+
+if(window.location.href == "https://www.facebook.com/"){
+    window.location.replace("https://www.facebook.com/messages");
+    GM_log("Redirected you to your messages");
+};
