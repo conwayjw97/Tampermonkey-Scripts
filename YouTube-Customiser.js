43d7aabfbdb21dbe48d9d2fa3bab244fdf33fea0
James <39833325+conwayjw97@users.noreply.github.com>
2022-02-04 22:26:05 +0000

    Update YouTube-Customiser.js

--- YouTube-Customiser.js
+++ YouTube-Customiser.js
@@ -5,12 +5,11 @@
 // @description  Ad removal and personal feature mods
 // @author       James Conway
 // @match        https://www.youtube.com/*
-// @grant        GM_log
 // @require      http://code.jquery.com/jquery-latest.js
 // @run-at       document-start
 // ==/UserScript==
 
-GM_log("YouTube Customiser is running");
+console.log("YouTube Customiser is running");
 
 let userHasScrolledUp = false;
 
@@ -29,6 +28,65 @@ function bottomScroll() {
     }, 1000);
 }
 
+function removeContents(){
+    let contents = document.getElementById("contents");
+    console.info(contents);
+    contents.remove();
+
+    let message = document.createElement("div");
+    message.innerHTML = "Same Old";
+    message.style.color = "white";
+    message.style.fontSize = "2000%";
+    message.style.display = "flex";
+    message.style.justifyContent = "center";
+    message.style.alignItems = "center";
+    document.getElementById("content").appendChild(message);
+    console.log("Deleted home page contents");
+}
+
+function addScrollDownButton(element){
+    let scrollToBottom = document.createElement("button");
+    scrollToBottom.innerHTML = "Scroll to Bottom (Scroll up to Stop)";
+    scrollToBottom.id = "ScrollToBottom";
+    scrollToBottom.onclick = function() {bottomScroll()};
+    document.getElementById(element).appendChild(scrollToBottom);
+    console.log("Added scroll to bottom button");
+}
+
+function removePlayerAds(){
+    let playerAds = document.getElementById("player-ads");
+    if(playerAds){
+        playerAds.remove();
+        console.log("Removed player side ad");
+    };
+}
+
+function toggleAutoplay(){
+    let autoplayToggle = document.getElementById("toggleButton");
+    if(autoplayToggle){
+        autoplayToggle.click();
+        console.log("Disabled autoplay");
+    };
+}
+
+function script(){
+    const url = window.location.href;
+
+    if(url == "https://www.youtube.com/"){
+        removeContents();
+    }
+
+    else if(url.startsWith("https://www.youtube.com/playlist")){
+        addScrollDownButton("alerts");
+    }
+
+    else if(url.startsWith("https://www.youtube.com/watch")){
+        removePlayerAds();
+        toggleAutoplay();
+        addScrollDownButton("info-contents");
+    }
+}
+
 window.onscroll = function (e){
     if(this.oldScroll > this.scrollY){
         userHasScrolledUp = true;
@@ -37,56 +95,9 @@ window.onscroll = function (e){
 }
 
 window.onload = function(){
-    let url = window.location.href;
-
-    if(url == "https://www.youtube.com/"){
-        let contents = document.getElementById("contents");
-        console.info(contents);
-        contents.remove();
-
-        let message = document.createElement("div");
-        message.innerHTML = "Same Old";
-        message.style.color = "white";
-        message.style.fontSize = "2000%";
-        message.style.display = "flex";
-        message.style.justifyContent = "center";
-        message.style.alignItems = "center";
-        console.info(message);
-        document.getElementById("content").appendChild(message);
-        GM_log("Deleted home page contents");
-    }
-
-    else if(url.startsWith("https://www.youtube.com/playlist")){
-        let scrollToBottom = document.createElement("button");
-        scrollToBottom.innerHTML = "Scroll to Bottom (Scroll up to Stop)";
-        scrollToBottom.id = "ScrollToBottom";
-        scrollToBottom.onclick = function() {bottomScroll()};
-        document.getElementById("alerts").appendChild(scrollToBottom);
-        GM_log("Added scroll to bottom button");
-    }
-
-    else if(url.startsWith("https://www.youtube.com/watch")){
-        // 2 second wait necessary as certain portions are only loaded dynamically
-        setTimeout(function(){
-            let playerAds = document.getElementById("player-ads");
-            if(playerAds){
-                playerAds.remove();
-                GM_log("Removed player side ad");
-            };
-
-            let autoplayToggle = document.getElementById("toggleButton");
-            if(autoplayToggle){
-                autoplayToggle.click();
-                GM_log("Disabled autoplay");
-            };
-
-            let scrollToBottom = document.createElement("button");
-            scrollToBottom.innerHTML = "Scroll to Bottom (Scroll up to Stop)";
-            scrollToBottom.id = "ScrollToBottom";
-            scrollToBottom.onclick = function() {bottomScroll()};
-            document.getElementById("info-contents").appendChild(scrollToBottom);
-            GM_log("Added scroll to bottom button");
-        }, 2000);
-    }
+    // 2 second wait necessary as certain portions are only loaded dynamically
+    setTimeout(function(){
+        script();
+    }, 2000);
 }
 
